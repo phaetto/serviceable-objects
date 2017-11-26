6ff8264cd84653023d2da30c6c0ac05ebbb05474
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2017-11-26 11:47:06 +0100

    Add a multiplatform test for Serviceable.Objects.Remote project

---
+++ Serviceable.Objects.Remote.MultiplePlatforms.Tests/TypesTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Serviceable.Objects.Remote.MultiplePlatforms.Tests
+{
+    using Dependencies;
+    using Objects.Composition;
+    using Xunit;
+
+    public class TypesTests
+    {
+        [Fact]
+        public void FindType_WhenFindingTypeWithAssemblyQualifiedName_ThenTypeIsFound()
+        {
+            var typeAssemblyQualifiedName = typeof(ContextGraph).AssemblyQualifiedName;
+
+            var type = Types.FindType(typeAssemblyQualifiedName);
+
+            Assert.Equal(typeof(ContextGraph), type);
+        }
+
+        [Fact]
+        public void FindType_WhenFindingTypeWithFullName_ThenTypeIsFound()
+        {
+            var typeFullName = typeof(ContextGraph).FullName;
+
+            var type = Types.FindType(typeFullName);
+
+            Assert.Equal(typeof(ContextGraph), type);
+        }
+    }
+}
