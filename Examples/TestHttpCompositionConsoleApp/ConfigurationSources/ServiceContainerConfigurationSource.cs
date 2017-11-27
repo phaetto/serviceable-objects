7898ba4ad8bf6ffdd23a5f359f5ea49d9dc4338a
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2017-11-27 21:47:32 +0100

    Add new configuration for ServiceContainer

---
+++ Examples/TestHttpCompositionConsoleApp/ConfigurationSources/ServiceContainerConfigurationSource.cs
@@ -0,0 +1,34 @@
+﻿namespace TestHttpCompositionConsoleApp.ConfigurationSources
+{
+    using System;
+    using Newtonsoft.Json;
+    using Serviceable.Objects.Composition.Graph;
+    using Serviceable.Objects.Composition.Graph.Stages.Configuration;
+    using Serviceable.Objects.Composition.Service;
+    using Serviceable.Objects.IO.NamedPipes.Server;
+    using Serviceable.Objects.IO.NamedPipes.Server.Configuration;
+    using Serviceable.Objects.Remote.Composition.ServiceContainer.Configuration;
+
+    public sealed class ServiceContainerConfigurationSource : IConfigurationSource
+    {
+        public string GetConfigurationValueForKey(IService service, GraphContext graphContext, GraphNodeContext graphNodeContext, Type type)
+        {
+            switch (type)
+            {
+                case var t when t == typeof(NamedPipeServerContext):
+                    return JsonConvert.SerializeObject(new NamedPipeServerConfiguration
+                    {
+                        PipeName  = "testpipe"
+                    });
+                case var t when t == typeof(ServiceContainerContextConfiguration):
+                    return JsonConvert.SerializeObject(new ServiceContainerContextConfiguration
+                    {
+                        ContainerName = "container-X",
+                        OrchestratorName = "orchestrator-X",
+                    });
+                default:
+                    throw new InvalidOperationException($"Type {type.FullName} is not supported.");
+            }
+        }
+    }
+}
