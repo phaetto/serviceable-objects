b5c6a726311e4e060cd584d12a9705c7951ae097
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2017-11-27 21:47:57 +0100

    First version of application host

---
+++ Serviceable.Objects.Remote/Composition/Host/Commands/RunAndBlock.cs
@@ -0,0 +1,19 @@
+﻿namespace Serviceable.Objects.Remote.Composition.Host.Commands
+{
+    using System;
+
+    public sealed class RunAndBlock : ICommand<ApplicationHost, ApplicationHost>
+    {
+        public ApplicationHost Execute(ApplicationHost context)
+        {
+            context.CancellationTokenSource.Token.Register(CancellationRequested, context);
+            context.EventWaitHandle.WaitOne();
+            return context;
+        }
+
+        private static void CancellationRequested(object context)
+        {
+            (context as ApplicationHost).EventWaitHandle.Set();
+        }
+    }
+}
