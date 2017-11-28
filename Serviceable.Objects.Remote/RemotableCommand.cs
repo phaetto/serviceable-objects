eb73e5acf94d6f893f83af67b83a0c5ad82a9782
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2017-11-28 23:13:38 +0100

    Removed not needed restriction

--- Serviceable.Objects.Remote/RemotableCommand.cs
+++ Serviceable.Objects.Remote/RemotableCommand.cs
@@ -1,10 +1,7 @@
 ﻿namespace Serviceable.Objects.Remote
 {
-    using Serviceable.Objects.Remote.Serialization;
-
     public abstract class RemotableCommand<TReceived, TContext> : Reproducible,
         IRemotableCommand<TContext, TReceived>
-        where TReceived : SerializableSpecification, new()
     {
         public abstract TReceived Execute(TContext context);
     }
