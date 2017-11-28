48901879640b2eb14fe1a3423d28cdddb8f6e620
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2017-11-28 23:12:44 +0100

    Add the container extensions that allow them to be externally configured

---
+++ Serviceable.Objects.Remote/Dependencies/DependencyInjectionRegistration.cs
@@ -0,0 +1,11 @@
+﻿namespace Serviceable.Objects.Remote.Dependencies
+{
+    public sealed class DependencyInjectionRegistration
+    {
+        public string Type { get; set; }
+
+        public bool WithDefaultInterface { get; set; }
+
+        public string WithInstanceType { get; set; }
+    }
+}
---
+++ Serviceable.Objects.Remote/Dependencies/DependencyInjectionRegistrationTemplate.cs
@@ -0,0 +1,9 @@
+﻿namespace Serviceable.Objects.Remote.Dependencies
+{
+    using System.Collections.Generic;
+
+    public sealed class DependencyInjectionRegistrationTemplate
+    {
+        public IEnumerable<DependencyInjectionRegistration> Registrations { get; set; }
+    }
+}
