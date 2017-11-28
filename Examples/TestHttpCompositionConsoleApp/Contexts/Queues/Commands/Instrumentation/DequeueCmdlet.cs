7b5b8e7a2e7ef6fe92d766065fc734e597895ef5
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2017-11-28 23:17:18 +0100

    Enable instrumentation on the enqueue-dequeue

---
+++ Examples/TestHttpCompositionConsoleApp/Contexts/Queues/Commands/Instrumentation/DequeueCmdlet.cs
@@ -0,0 +1,14 @@
+﻿namespace TestHttpCompositionConsoleApp.Contexts.Queues.Commands.Instrumentation
+{
+    using System.Management.Automation;
+    using Serviceable.Objects.Instrumentation;
+
+    [Cmdlet("Dequeue", "Message")]
+    public sealed class DequeueCmdlet : InstrumentationCommandCmdlet<Dequeue>
+    {
+        public override Dequeue GenerateCommand()
+        {
+            return new Dequeue();
+        }
+    }
+}
---
+++ Examples/TestHttpCompositionConsoleApp/Contexts/Queues/Commands/Instrumentation/EnqueueCmdlet.cs
@@ -0,0 +1,19 @@
+﻿namespace TestHttpCompositionConsoleApp.Contexts.Queues.Commands.Instrumentation
+{
+    using System.Management.Automation;
+    using Data;
+    using Serviceable.Objects.Instrumentation;
+
+    [Cmdlet("Enqueue", "Message")]
+    public sealed class EnqueueCmdlet : InstrumentationCommandCmdlet<Enqueue>
+    {
+        [ValidateNotNullOrEmpty]
+        [Parameter(Mandatory = true, Position = 0, HelpMessage = "The message you would like to enqueue")]
+        public string Message { get; set; }
+
+        public override Enqueue GenerateCommand()
+        {
+            return new Enqueue(new QueueItem { Data = Message });
+        }
+    }
+}
