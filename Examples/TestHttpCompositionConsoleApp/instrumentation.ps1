bb12a051b1c760c5fd2cedbb03c04b52c12270cf
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2017-12-01 10:46:18 +0100

    Update the test with the optional copy for the 'System.Runtime 4.1.0' error requirement subsystem.

--- Examples/TestHttpCompositionConsoleApp/instrumentation.ps1
+++ Examples/TestHttpCompositionConsoleApp/instrumentation.ps1
@@ -1,4 +1,6 @@
-﻿$location = Split-Path $MyInvocation.MyCommand.Path;
+﻿$ErrorActionPreference = "Stop"
+
+$location = Split-Path $MyInvocation.MyCommand.Path;
 
 PowerShell -NoProfile -Command {
     param (
@@ -6,16 +8,20 @@ PowerShell -NoProfile -Command {
     )
 
     $appRuntime = "netcoreapp1.0";
-    $folder = "$location\bin\Debug\$appRuntime";
-    Set-Location -Path $folder;
+    Set-Location -Path $location;
+    #Copy-Item ..\..\..\powershell-binaries-$appRuntime\* -Destination . -Force;
 
-    # Copy-Item C:\sources\powershell-binaries-$appRuntime\* -Destination $folder -Force;
+    $folder = "$location\bin\Debug\$appRuntime";
+    #$folder = "$location\bin\Release\$appRuntime\win-x64\publish";
+    Set-Location -Path $folder;
+    Move-Item "$location\*.dll" $folder -Force;
 
     Import-Module "$folder\TestHttpCompositionConsoleApp.dll";
 
     #Get-Help Write-Message;
     #Get-Help Enqueue-Message;
 
+    # This is an error
     #$message = Dequeue-Message -ServiceOrchestrator "orchestrator-X" -ServiceName "service-X" -ContextId "LALALA- queue-context";
     
     Write-Host "Message 1: '$message'";
