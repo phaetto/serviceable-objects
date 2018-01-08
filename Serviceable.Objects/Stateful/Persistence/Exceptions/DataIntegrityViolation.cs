31372e66990d02e89c806d2b0a9b5357c7330600
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2018-01-08 18:06:48 +0100

    Add the persistence base-concept objects

---
+++ Serviceable.Objects/Stateful/Persistence/Exceptions/DataIntegrityViolation.cs
@@ -0,0 +1,21 @@
+﻿namespace Serviceable.Objects.Stateful.Persistence.Exceptions
+{
+    using System;
+
+    public class DataIntegrityViolationException : Exception
+    {
+        public DataIntegrityViolationException()
+        {
+        }
+
+        public DataIntegrityViolationException(string message)
+            : base(message)
+        {
+        }
+
+        public DataIntegrityViolationException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ Serviceable.Objects/Stateful/Persistence/IPersistentEventStore.cs
@@ -0,0 +1,18 @@
+﻿namespace Serviceable.Objects.Stateful.Persistence
+{
+    using System;
+
+    public interface IPersistentEventStore<in T>
+        where T : IEvent
+    {
+        DateTimeOffset? GetLastEventTime(T data);
+
+        IEvent[] LoadAppliedEvents(T data, DateTimeOffset? fromDateTimeOffset = null);
+
+        void CreateEvent(T data);
+
+        void AppendEvent(T data);
+
+        void DeleteEvent(T data);
+    }
+}
