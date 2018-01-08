5fcd7eba9750109665fcabe8f8d9f8b70f46ad7f
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2018-01-08 18:45:09 +0100

    Add a stub instrumentation for closing a service (nneds more work later)

---
+++ Serviceable.Objects.Instrumentation/Composition/Service/Commands/NewServiceCmdlet.cs
@@ -0,0 +1,11 @@
+﻿namespace Serviceable.Objects.Instrumentation.Composition.Service.Commands
+{
+    using System.Management.Automation;
+    using Powershell;
+    using Remote.Composition.Service.Commands;
+
+    [Cmdlet(VerbsCommon.Close, "Service")]
+    public sealed class CloseServiceCmdlet : InstrumentationCommandCmdlet<CloseService>
+    {
+    }
+}
---
+++ Serviceable.Objects.Remote/Composition/Service/Commands/CloseService.cs
@@ -0,0 +1,17 @@
+﻿namespace Serviceable.Objects.Remote.Composition.Service.Commands
+{
+    using Host;
+
+    public class CloseService : ReproducibleCommand<ServiceContext, ServiceContext>
+    {
+        public override ServiceContext Execute(ServiceContext context)
+        {
+            //TODO: shut down the service gracefully
+
+            var applicationHost = context.ServiceContainer.Resolve<ApplicationHost>();
+            applicationHost.CancellationTokenSource.Cancel();
+
+            return context;
+        }
+    }
+}
