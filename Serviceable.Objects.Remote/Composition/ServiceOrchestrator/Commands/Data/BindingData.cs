eced4fc02be6af8b89f297d921d0cb916497b864
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2018-01-08 18:21:14 +0100

    Add instrumentation for bindings

---
+++ Serviceable.Objects.Remote/Composition/ServiceOrchestrator/Commands/Data/BindingData.cs
@@ -0,0 +1,12 @@
+﻿namespace Serviceable.Objects.Remote.Composition.ServiceOrchestrator.Commands.Data
+{
+    using System.Collections.Generic;
+    using Objects.Composition.ServiceOrchestrator;
+
+    public sealed class BindingData
+    {
+        public string ServiceName { get; set; }
+        public IEnumerable<InBinding> InBindings { get; set; }
+        public IEnumerable<ExternalBinding> ExternalBindings { get; set; }
+    }
+}
---
+++ Serviceable.Objects.Remote/Composition/ServiceOrchestrator/Commands/SetBinding.cs
@@ -0,0 +1,19 @@
+﻿namespace Serviceable.Objects.Remote.Composition.ServiceOrchestrator.Commands
+{
+    using System.Linq;
+    using Data;
+
+    public sealed class SetBinding : ReproducibleCommandWithData<ServiceOrchestratorContext, ServiceOrchestratorContext, BindingData>
+    {
+        public SetBinding(BindingData data) : base(data)
+        {
+        }
+
+        public override ServiceOrchestratorContext Execute(ServiceOrchestratorContext context)
+        {
+            context.InBindingsPerService[Data.ServiceName] = Data.InBindings.ToList();
+            context.ExternalBindingsPerService[Data.ServiceName] = Data.ExternalBindings.ToList();
+            return context;
+        }
+    }
+}
