e530eef471f62ef25fe8684acdf9c66e075fc43b
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2018-01-23 20:14:33 +0100

    Add start and stop commands for service-host

---
+++ Serviceable.Objects.Windows/ServiceCommands/Start.cs
@@ -0,0 +1,12 @@
+﻿namespace Serviceable.Objects.Windows.ServiceCommands
+{
+    using Remote.Composition.Host;
+    public sealed class Start : ICommand<ApplicationHost, ApplicationHost>
+    {
+        public ApplicationHost Execute(ApplicationHost context)
+        {
+            context.GraphContext.ConfigureSetupAndInitialize();
+            return context;
+        }
+    }
+}
